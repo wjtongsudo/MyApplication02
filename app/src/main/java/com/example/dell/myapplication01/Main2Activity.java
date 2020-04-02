2e4c9e920b1690bcf8d5f5822a7de5b7ea84e9bb
wjtongsudo <501877929@qq.com>
2020-04-02 12:25:15 +0800

    reason

--- app/src/main/java/com/example/dell/myapplication01/Main2Activity.java
+++ app/src/main/java/com/example/dell/myapplication01/Main2Activity.java
@@ -1,13 +1,21 @@
 package com.example.dell.myapplication01;
 
+import android.app.WallpaperManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-protected void onCreate(Bundle savedInstanceState) {
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //加载界面
-        setContentView(R.layout.layout_chu4_1);
+        setContentView(R.layout.layout_ch4_1);
 
         //1)获取普通界面组件,通过id找按钮
         Button button=(Button) findViewById(R.id.button1);
@@ -18,30 +26,30 @@ protected void onCreate(Bundle savedInstanceState) {
 
         //使用内部匿名类注册监听器
         imageView.setOnLongClickListener(new View.OnLongClickListener() {
-@Override
-public boolean onLongClick(View view) {
-        //设置壁纸
-        WallpaperManager wallpaperManager=(WallpaperManager) getSystemService(WALLPAPER_SERVICE);
-        try{
-        wallpaperManager.setResource(R.raw.img);
-        }catch (Exception e){
-        Log.e(Main2Activity.class.toString(),e.toString());
-        }
+            @Override
+            public boolean onLongClick(View view) {
+                //设置壁纸
+                WallpaperManager wallpaperManager=(WallpaperManager) getSystemService(WALLPAPER_SERVICE);
+                try{
+                    wallpaperManager.setResource(R.raw.img);
+                }catch (Exception e){
+                    Log.e(Main2Activity.class.toString(),e.toString());
+                }
 
-        return false;
-        }
+                return false;
+            }
         });
 
 
+    }
+    //2)实现事件监听类
+    class MyClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            Log.i(Main2Activity.class.toString(),"button click");
+
         }
-//2)实现事件监听类
-class MyClickListener implements View.OnClickListener{
-    @Override
-    public void onClick(View view) {
-        Log.i(Main2Activity.class.toString(),"button click");
+
 
     }
-
-
-}
 }
